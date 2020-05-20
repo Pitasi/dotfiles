87aac175059c5ea84de773d581bf368fe0ca5439
Antonio Pitasi <me@anto.pt>
2020-05-20 19:46:08 +0200

    Update dot_config/i3/config Add dot_config/i3/scripts/executable_bingpaper.py

---
+++ dot_config/i3/scripts/executable_bingpaper.py
@@ -0,0 +1,34 @@
+#!/usr/bin/env python
+
+from pathlib import Path
+from time import sleep
+import json
+import shutil
+import subprocess
+import requests
+
+def try_get(*args, **kwargs):
+    e = None
+    for i in range(5):
+        try:
+            return requests.get(*args, **kwargs)
+        except Exception as exc:
+            e = exc
+            sleep(1000)
+    raise e
+
+def image_url():
+    u = "https://www.bing.com/HPImageArchive.aspx?idx=0&format=js&mkt=it-IT&n=1"
+    res = try_get(u)
+    data = res.json()
+    img_path = data['images'][0]['url']
+    return "https://www.bing.com" + img_path
+
+img_url = image_url()
+response = try_get(img_url, stream=True)
+
+out_path = Path.home() / Path(".local/share/bingpaper.jpg")
+with out_path.open('wb') as out:
+    shutil.copyfileobj(response.raw, out)
+
+subprocess.run(["wal", "-i", str(out_path)])
