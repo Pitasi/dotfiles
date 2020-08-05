ed0a48bbcc07883d183b752b4f906db502d066a8
Antonio Pitasi <me@anto.pt>
2020-08-05 12:10:18 +0200

    Update dot_config/fish/executable_config.fish

--- dot_config/fish/executable_config.fish
+++ dot_config/fish/executable_config.fish
@@ -11,6 +11,15 @@ if command -v tmux > /dev/null && ! set -q TMUX && status is-interactive
     exec tmux new-session -A -s default
 end
 
+# Enable vi bindings + tweaks to still use Ctrl-F, Ctrl-N, Ctrl-P
+fish_vi_key_bindings
+for mode in insert default visual
+	bind -M $mode \cf forward-char
+    bind -M $mode \cp up-or-search
+    bind -M $mode \cn down-or-search
+end
+
+
 # tabtab source for packages
 # uninstall by removing these lines
 [ -f ~/.config/tabtab/__tabtab.fish ]; and . ~/.config/tabtab/__tabtab.fish; or true
